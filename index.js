58c82fbb9c72df5f854ae30173dd976b6dcb4356
fjktkm <fjktkm@gmail.com>
2023-08-06 18:16:38 +0900

    chore: Azure App Service でデプロイするため環境変数の処理を変更

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+
+// const { token } = require('./config.json');
+const token = process.env.DISCORD_TOKEN;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
